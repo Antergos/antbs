5d280a9e3397257bf5c8f28ce134365fc34d6c6c
Dustin Falgout <dustin@falgout.us>
2017-05-13 13:47:41 -0500

    Add gpg keys for nodejs releases

--- antbs/build/build.sh
+++ antbs/build/build.sh
@@ -130,10 +130,20 @@ fetch_upstream_pgp_keys() {
 	[[ -n $1 ]] && fake_home="$1"
 
 	# Mozilla Software Releases <release@mozilla.com>
-	mozilla='14F26682D0916CDD81E37B6D61B7B526D98F0353'
+	mozilla=(
+		'14F26682D0916CDD81E37B6D61B7B526D98F0353'
+		'F2EF4E6E6AE75B95F11F1EB51C69C4E55E9905DB'
+	)
+
+	nodejs=(
+		'94AE36675C464D64BAFA68DD7434390BDBE9B9C5' 'FD3A5288F042B6850C66B31F09FE44734EB7990E'
+		'71DCFD284A79C3B38668286BC97EC7A07EDE3FC1' 'DD8F2338BAE7501E3DD5AC78C273792F7D83545D'
+		'C4F0DFFF4E8C1A8236409D08E73BC641CC11F4C8' 'B9AE9905FFD7803F25714661B63B535A4C206CA9'
+		'56730D5401028683275BD23C23EFEFE93C4CFFFE'
+	)
 
 	install -dm777 "${fake_home}" # gpg needs $HOME to exist and be writable
-	sudo -H -u antbs gpg --keyserver pool.sks-keyservers.net --recv-keys "${mozilla}" 2>&1
+	sudo -H -u antbs gpg --keyserver pool.sks-keyservers.net --recv-keys "${mozilla[@]}" "${nodejs[@]}" 2>&1
 }
 
 in_array() {
