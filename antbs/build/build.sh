b4c028ba94b0a0b6ca17c6a6c7f15c37a8190630
Dustin Falgout <dustin@falgout.us>
2017-06-09 02:48:39 -0500

    update keyserver address

--- antbs/build/build.sh
+++ antbs/build/build.sh
@@ -136,14 +136,14 @@ fetch_upstream_pgp_keys() {
 	)
 
 	nodejs=(
-		'94AE36675C464D64BAFA68DD7434390BDBE9B9C5' 'FD3A5288F042B6850C66B31F09FE44734EB7990E'
+		'94AE36675C464D64BAFA68DD7434390BDBE9B9C5' 'FD3A528q8F042B6850C66B31F09FE44734EB7990E'
 		'71DCFD284A79C3B38668286BC97EC7A07EDE3FC1' 'DD8F2338BAE7501E3DD5AC78C273792F7D83545D'
 		'C4F0DFFF4E8C1A8236409D08E73BC641CC11F4C8' 'B9AE9905FFD7803F25714661B63B535A4C206CA9'
 		'56730D5401028683275BD23C23EFEFE93C4CFFFE'
 	)
 
 	install -dm777 "${fake_home}" # gpg needs $HOME to exist and be writable
-	sudo -H -u antbs gpg --keyserver hkp://keyserver.kjsl.com:80 --recv-keys "${mozilla[@]}" "${nodejs[@]}" 2>&1
+	sudo -H -u antbs gpg --keyserver hkp://p80.pool.sks-keyservers.net:80 --recv-keys "${mozilla[@]}" "${nodejs[@]}" 2>&1
 }
 
 in_array() {
